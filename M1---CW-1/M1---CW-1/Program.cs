d5de59f59d7a091f080c9c40fbe3536a7748b825
mustaphography <mustaphography@gmail.com>
2020-12-20 19:30:00 +0300

    Done

---
+++ M1---CW-1/M1---CW-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace M1___CW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String characterName = "Ahmad";
+            String superPower1 = "steel armor";
+            String superPower2 = "night vision";
+            int age = 15;
+            double speed = 1.65;
+
+            Console.WriteLine("My name is " +  characterName + " and my super powers are " + superPower1 + " and " + superPower2 + " . My age is " + age + " and my speed is " + speed);
+            Console.WriteLine("");
+            Console.WriteLine("My name is {0} and my super powers are {1} and {2} . My age is {3} and my speed is {4}", characterName, superPower1, superPower2, age, speed);
+            
+        }
+    }
+}
